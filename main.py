2eb7ee00135db00094cd2098fb1c35765eb351b7
chen2233 <57244601+chen2233@users.noreply.github.com>
2020-07-06 15:09:24 +0800

    correct missing ','

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ api_list = [
     r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',
     r'https://graph.microsoft.com/v1.0/me/mailFolders',
     r'https://graph.microsoft.com/v1.0/me/onenote/sections',
-    r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories'
+    r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories',
     r'https://graph.microsoft.com/v1.0/users',
 ]
 
